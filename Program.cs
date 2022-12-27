b4f6191a7cd603f3539474737862a6957cd75b69
lucavgobbi <gobbi@me.com>
2022-12-27 14:05:41 -0500

    First commit with Statiq

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿await Bootstrapper
+    .Factory
+    .CreateWeb(args)
+    .DeployToGitHubPagesBranch(
+        "lucavgobbi",
+        "lucavgobbi.github.io",
+        Config.FromSetting<string>("GITHUB_TOKEN"),
+        "main")
+    .RunAsync()
+    .ConfigureAwait(false);
---
+++ input/index.md
@@ -0,0 +1,5 @@
+Title: lucavgobbi.github.io
+---
+# Hello World!
+
+Hello from my first github page built with Statiq.
